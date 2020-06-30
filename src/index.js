a07db20ae47c6901484a6c11afdd29a958630983
Matias <matias.rondan@globant.com>
2020-06-30 15:37:47 -0300

    add error and not found middlewares

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
+/* eslint-disable no-console */
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const middlewares = require('./middlewares');
+
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
-    origin: 'http://localhost:3000',
+  origin: 'http://localhost:3000',
 }));
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'hello again',
+  });
+});
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 2204;
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`)
+  console.log(`Listening at http://localhost:${port}`);
 });
